69b5dfbef9a0e2b70ebde3b50b9ae1586c100fd6
harry <harry@alphashed.cn>
2024-08-02 20:39:24 +0800

    24/08/02

---
+++ P1022/main.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+double k, b, now;
+char tmp, x;
+bool pm = true, a = true, last = false;
+
+int main()
+{
+    while (cin >> tmp)
+    {
+        if (tmp == '+')
+        {
+            if (a == true)
+            {
+                pm = true;
+            }
+            else pm = false;
+            last = false;
+        }
+        else if (tmp == '-')
+        {
+            if (a == true)
+            {
+                pm = false;
+            }
+            else pm = true;
+            last = false;
+        }
+        else if (tmp == '=')
+        {
+            a = false;
+            pm = false;
+            last = false;
+        }
+        else if (tmp >= '0' && tmp <= '9')
+        {
+            if (last == true)
+            {
+                if (pm)
+                {
+                    now = now * 10 + (tmp - '0');
+                }
+                else now = now * 10 - (tmp - '0');
+            }
+            else
+            {
+                if (pm) now = tmp - '0';
+                else now = -(tmp - '0');
+                last = true;
+            }
+        }
+        else
+        {
+            x = tmp;
+            if (now == 0 && !last) now = 1;
+            k += now;
+            last = false;
+            now = 0;
+        }
+        if (!last)
+        {
+            b += now;
+            now = 0;
+        }
+    }
+    b += now;
+    double ans = -b / k;
+    if (ans == -0.0) ans = 0;
+    cout << fixed << setprecision(3);
+    cout << x << "=" << ans;
+    return 0;
+}
--- P1045/main.cpp
+++ P1045/main.cpp
@@ -1,7 +1,65 @@
 ﻿#include <cstdio>
+#include <cmath>
+#include <cstring>
+using namespace std;
+
+int p, sav[1001], res[1001], f[1001];
+
+void result_1()
+{
+    memset(sav, 0, sizeof(sav));
+    for (int i = 1; i <= 500; i++)
+    {
+        for (int j = 1; j <= 500; j++)
+        {
+            sav[i + j - 1] += res[i] * f[j];
+        }
+    }
+    for (int i = 1; i <= 500; i++)
+    {
+        sav[i + 1] += sav[i] / 10;
+        sav[i] %= 10;
+    }
+    memcpy(res, sav, sizeof(res));
+}
+
+void result_2()
+{
+    memset(sav, 0, sizeof(sav));
+    for (int i = 1; i <= 500; i++)
+    {
+        for (int j = 1; j <= 500; j++)
+        {
+            sav[i + j - 1] += f[i] * f[j];
+        }
+    }
+    for (int i = 1; i <= 500; i++)
+    {
+        sav[i + 1] += sav[i] / 10;
+        sav[i] %= 10;
+    }
+    memcpy(f, sav, sizeof(f));
+}
 
 int main()
 {
-    
+    scanf("%d", &p);
+    printf("%d", (int)(log10(2) * p + 1)); 
+    res[1] = 1;
+    f[1] = 2;
+    while (p)
+    {
+        if (p % 2 == 0)
+        {
+            result_1();
+        }
+        p /= 2;
+        result_2();
+    }
+    for (int i = 500; i > 0; i--)
+    {
+        if (i % 50 == 0) printf("\n%d", res[i]);
+        else printf("%d", res[i]);
+    }
     return 0;
 }
