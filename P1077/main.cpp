13ad2032a30dde1b7740bb5016f638a86fcb0d18
HugeSingularity <harry@alphashed.cn>
2024-08-03 16:35:53 +0800

    24/08/03

---
+++ P1077/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <cstdio>
+
+int a[101], b[101][101];
+
+int main()
+{
+    int n, m;
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d", a + i);
+    }
+    for (int i = 0; i <= n; i++) b[i][0] = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            for (int k = j; k >= j - a[i]; k--)
+            {
+                if (k >= 0)
+                {
+                    b[i][j] += b[i - 1][k] % 1000007;
+                    b[i][j] %= 1000007;
+                }
+                else break;
+            }
+        }
+    }
+    printf("%d", b[n][m]);
+    return 0;
+}
