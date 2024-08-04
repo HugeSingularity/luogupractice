9f75d8b36e12cba37a435149ae7fcca204d16ba7
HugeSingularity <harry@alphashed.cn>
2024-08-04 14:55:35 +0800

    24/08/04

---
+++ P1096/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <cstdio>
+
+int step[100];
+
+int main()
+{
+    int n, l = 0;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= l; j++)
+        {
+            step[j] = 2 * step[j];
+        }
+        step[0] += 2;
+        for (int i = 0; i < l; i++)
+        {
+            step[i + 1] += step[i] / 10;
+            step[i] %= 10;
+        }
+        for (;;)
+        {
+            if (step[l] > 9)
+            {
+                step[l + 1] += step[l] / 10;
+                step[l++] %= 10;
+            }
+            else break;
+        }
+    }
+    for (int i = l; i >= 0; i--)
+    {
+        printf("%d", step[i]);
+    }
+    return 0;
+}
