4d7fe93d47443f4f129157833b92185fe204bc9b
HugeSingularity <harry@alphashed.cn>
2024-08-04 21:05:56 +0800

    24/08/04

---
+++ P1002/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <cstdio>
+
+struct Place
+{
+    int x = 0, y = 0;
+};
+
+Place soldier, horse, goal;
+int ans;
+
+void dfs()
+{
+    if (soldier.x == goal.x && soldier.y == goal.y)
+    {
+        ans++;
+        return;
+    }
+    if ((soldier.x == horse.x + 1 && soldier.y == horse.y + 2) || (soldier.x == horse.x + 2 && soldier.y == horse.y + 1) || (soldier.x == horse.x + 2 && soldier.y == horse.y - 1) || (soldier.x == horse.x + 1 && soldier.y == horse.y - 2) || (soldier.x == horse.x - 1 && soldier.y == horse.y - 2) || (soldier.x == horse.x - 2 && soldier.y == horse.y - 1) || (soldier.x == horse.x - 2 && soldier.y == horse.y + 1) || (soldier.x == horse.x - 1 && soldier.y == horse.y + 2) || (soldier.x == horse.x && soldier.y == horse.y)) return;
+    if (soldier.x + 1 <= goal.x)
+    {
+        soldier.x++;
+        dfs();
+        soldier.x--;
+    }
+    if (soldier.y + 1 <= goal.y)
+    {
+        soldier.y++;
+        dfs();
+        soldier.y--;
+    }
+    return;
+}
+
+int main()
+{
+    scanf("%d%d%d%d", &goal.y, &goal.x, &horse.y, &horse.x);
+    dfs();
+    printf("%d", ans);
+    return 0;
+}
