b071b3c515b966a64708b89db999f24bfb6ed7ee
HugeSingularity <harry@alphashed.cn>
2024-08-10 19:33:21 +0800

    24/08/10

---
+++ P1311/main.cpp
@@ -0,0 +1,22 @@
+﻿#include<cstdio>
+
+using namespace std;
+
+int n, k, p, t, ans, price, num[110], color[200100];
+
+int main()
+{
+	scanf("%d%d%d", &n, &k, &p);
+	for (int i = 1; i <= n; i++)
+	{
+		scanf("%d%d", &color[i], &price);
+		if (price <= p)
+		{
+			for (int j = i; j > t; j--) num[color[j]]++;
+			t = i, ans += num[color[i]] - 1;
+		}
+		else ans += num[color[i]];
+	}
+	printf("%d", ans);
+	return 0;
+}
