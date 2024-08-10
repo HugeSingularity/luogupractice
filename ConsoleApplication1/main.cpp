92e17efce376e82dc211b4e446061aecda93d5f5
HugeSingularity <harry@alphashed.cn>
2024-08-10 19:14:15 +0800

    24/08/10

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -1,23 +1,16 @@
-﻿#include<cstdio>
-#include<algorithm>
-using std::sort;
-int a[510][510];
+﻿#include <algorithm>
+#include <cstdio>
+
+using namespace std;
+
+int a[10];
+
+bool bigger(int a, int b)
+{
+	if (a > b) 
+}
+
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    for (int i = 1; i < n; i++)
-        for (int j = i + 1; j <= n; j++)
-        {
-            scanf("%d", &a[i][j]);
-            a[j][i] = a[i][j];
-        }
-    int ans = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        sort(a[i] + 1, a[i] + 1 + n);
-        ans = ans > a[i][n - 1] ? ans : a[i][n - 1];//选出排名第二中最大的那个
-    }
-    printf("1\n%d\n", ans);//一定有解
-    return 0;
+	
 }
---
+++ P1309/main.cpp
@@ -0,0 +1,64 @@
+﻿#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int n, r, q;
+
+struct competitor
+{
+    long long power, number, score;
+    bool operator<(const competitor& t)
+        const {
+        if (t.score == score) return number < t.number;
+        return score > t.score;
+    }
+}a[200000], loser[200000], winner[200000];
+
+bool bigger(competitor x, competitor y)
+{
+    if (x.score == y.score) return x.number < y.number;
+    return x.score > y.score;
+}
+
+void fighting()
+{
+    for (int i = 0; i < 2 * n; i += 2)
+    {
+        if (a[i].power > a[i + 1].power)
+        {
+            a[i].score++;
+            loser[i / 2] = a[i + 1];
+            winner[i / 2] = a[i];
+        }
+        else
+        {
+            a[i + 1].score++;
+            loser[i / 2] = a[i];
+            winner[i / 2] = a[i + 1];
+        }
+    }
+    return;
+}
+
+int main()
+{
+    scanf("%d%d%d", &n, &r, &q);
+    for (int i = 0; i < 2 * n; i++)
+    {
+        a[i].number = i + 1;
+        scanf("%d", &a[i].score);
+    }
+    for (int i = 0; i < 2 * n; i++)
+    {
+        scanf("%d", &a[i].power);
+    }
+    sort(a, a + 2 * n);
+    for (int i = 0; i < r; i++)
+    {
+        fighting();
+        merge(loser, loser + n, winner, winner + n, a);
+    }
+    printf("%lld", a[q - 1].number);
+    return 0;
+}
