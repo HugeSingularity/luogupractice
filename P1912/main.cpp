642e6672ad679682e2014fcd763e5c6403dc4daa
HugeSingularity <harry@alphashed.cn>
2024-08-11 16:47:34 +0800

    24/08/11

--- P1912/main.cpp
+++ P1912/main.cpp
@@ -1,20 +1,24 @@
 ﻿#include<iostream>
 #include<algorithm>
 #include<cstring>
+
 using namespace std;
-bool cmp(string a, string b) {
+
+bool cmp(string a, string b)
+{
 	return a + b > b + a;
 }
 
-int main() {
+int main()
+{
 	int n;
 	cin >> n;
 	string a[n];
 	for (int i = 0; i < n; i++) cin >> a[i];
 	sort(a, a + n, cmp);
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++)
+	{
 		cout << a[i];
 	}
-	cout << endl;
 	return 0;
 }
