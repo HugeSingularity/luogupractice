bc08e63b8e2d3a26b75ad96b3dd9787f977e782c
HugeSingularity <harry@alphashed.cn>
2024-08-11 11:04:18 +0800

    24/08/11

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -1,16 +1,46 @@
-﻿#include <algorithm>
-#include <cstdio>
-
+﻿#include<iostream>
+#include<cstdio>
 using namespace std;
-
-int a[10];
-
-bool bigger(int a, int b)
-{
-	if (a > b) 
-}
-
+int g[105], f[105], a[105], s[105];
 int main()
 {
-	
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> a[i];
+		f[i] = 1;
+		g[i] = 1;
+	}//输入并赋初值 
+	for (int i = n - 1; i >= 1; i--)
+	{
+		for (int j = i + 1; j <= n; j++)
+		{
+			if (a[i] > a[j] && f[i] <= f[j] + 1)
+			{
+				f[i] = f[j] + 1;
+			}
+		}
+	}//从右往左，按左高右低顺序找出每一个位置右边有几个从高到低的数 （包括自己） 
+	for (int i = 2; i <= n; i++)
+	{
+		for (int j = 1; j < i; j++)
+		{
+			if (a[i] > a[j] && g[i] <= g[j] + 1)
+			{
+				g[i] = g[j] + 1;
+			}
+		}
+	}//从左往右，按左低右高顺序找出每一个位置左边有几个从低到高的数 （包括自己） 
+	int maxx = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		s[i] = f[i] + g[i] - 1;//把每个数的左边从低到高的数和右边从高到低的数相加 
+		//注意！！自己加了两次要-1 
+		if (s[i] > maxx)
+		{
+			maxx = s[i];
+		}
+	}
+	cout << n - maxx;//是求出列的人数 
 }
---
+++ P1091/main.cpp
@@ -0,0 +1,47 @@
+﻿#include <cstdio>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int l[n], r[n], all[n], t[n];
+    memset(l, 0, sizeof(l));
+    memset(r, 0, sizeof(r));
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", t + i);
+        r[i] = 1;
+        l[i] = 1;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int i = n - 2; i >= 0; i--)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (t[i] > t[j] && r[i] <= r[j] + 1)
+                {
+                    r[i] = r[j] + 1;
+                }
+            }
+        }
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (t[i] > t[j] && l[i] <= l[j] + 1)
+                {
+                    l[i] = l[j] + 1;
+                }
+            }
+        }
+        all[i] = l[i] + r[i] - 1;
+    }
+    sort(all, all + n);
+    printf("%d", n - all[n - 1]);
+    return 0;
+}
