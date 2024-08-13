b8fb50837edd7dcbcfc2f58c3f5ce03cb49a874b
harry <harry@alphashed.cn>
2024-08-13 17:47:21 +0800

    24/08/13

---
+++ P1090/main.cpp
@@ -0,0 +1,44 @@
+﻿#include <cstdio>
+#include <algorithm>
+#include <cstring>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int t = n - 1;
+    int pile1[n], pile2[n];
+    memset(pile2, 0, sizeof(pile2));
+    for (int i = 0; i < n; i++)
+        scanf("%d", pile1 + i);
+    std::sort(pile1, pile1 + n);
+    int i = 0, j = 0, cnt = -1, total = 0;
+
+    while (t--)
+    {
+        int tmp = 0;
+        if (i == n || pile1[i] > pile2[j] && j <= cnt)
+        {
+            tmp += pile2[j++];
+        }
+        else
+        {
+            tmp += pile1[i++];
+        }
+
+        if (i == n || pile1[i] > pile2[j] && j <= cnt)
+        {
+            tmp += pile2[j++];
+        }
+        else
+        {
+            tmp += pile1[i++];
+        }
+        pile2[++cnt] = tmp;
+        total += tmp;
+    }
+
+    printf("%d", total);
+
+    return 0;
+}
