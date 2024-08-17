2c93df8bba3b28b0967637ffd5b56952bece9d09
harry <harry@alphashed.cn>
2024-08-17 21:12:16 +0800

    24/08/17

---
+++ P1033/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <cstdio>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+double h, s1, v, l, k;
+
+int main()
+{
+    scanf ("%lf%lf%lf%lf%lf%d", &h, &s1, &v, &l, &k, &n);
+    double tMax = sqrt(h / 5);
+    double tMin = sqrt((h - k) / 5);
+    int i1 = int(s1 - tMin * v + l), i2 = int(s1 - tMax * v);
+    i1 = min(i1, n);
+    i2 = max(i2, 0);
+    printf("%d", i1 - i2);
+    return 0;
+}
---
+++ P1969/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <cstdio>
+
+int h, n, ans, now;
+
+int main()
+{
+    scanf("%d", &n);
+    scanf("%d", &ans);
+    now = ans;
+    for (int i = 1; i < n; i++)
+    {
+        scanf("%d", &h);
+        if (h > now) ans += (h - now);
+        now = h;
+    }
+    printf("%d", ans);
+    return 0;
+}
