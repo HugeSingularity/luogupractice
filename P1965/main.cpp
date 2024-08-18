6c350fa5f74cb5c500ed73f736ac456f3537ef99
HugeSingularity <harry@alphashed.cn>
2024-08-18 09:16:52 +0800

    24/08/18

---
+++ P1965/main.cpp
@@ -0,0 +1,23 @@
+﻿#include<cstdio>
+
+int n, m, k, x, a[1000005];
+
+int main()
+{
+    scanf("%d%d%d%d", &n, &m, &k, &x);
+    long long i; 
+    a[0] = x;
+    for (i = 1;; i++) 
+    {
+        x += m;
+        if (x >= n) x -= n;
+        a[i] = x;
+        if (x == a[0]) break;
+    }
+    int l = k / 10, r = k % 10;
+    int q = 1;
+    for (int j = 0; j < l; j++) q = q * 10000000000 % i;
+    for (int j = 0; j < r; j++) q = q * 10 % i;
+    printf("%d", a[q]);
+    return 0;
+}
---
+++ P1970/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int n, h[100010], f[100010][2];
+
+int main() 
+{
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) scanf("%d", &h[i]);
+    f[0][0] = f[0][1] = 1;
+    for (int i = 1; i < n; i++) {
+        if (h[i] > h[i - 1]) f[i][0] = f[i - 1][1] + 1;
+        else f[i][0] = f[i - 1][0];
+        if (h[i] < h[i - 1]) f[i][1] = f[i - 1][0] + 1;
+        else f[i][1] = f[i - 1][1];
+    }
+    printf("%d", max(f[n - 1][0], f[n - 1][1]));
+    return 0;
+}
